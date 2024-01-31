6ff5378b1efe1c5d9dd8af34da6625891a64dab3
ngoctan2101 <109189357+ngoctan2101@users.noreply.github.com>
2024-01-31 20:18:34 +0700

    Revert "Imapper"
    This reverts commit 6aa641b473235f4b4bf7a1593f71b85f82e5a19d.

--- OpusLink.API/Program.cs
+++ OpusLink.API/Program.cs
@@ -1,7 +1,3 @@
-using AutoMapper;
-using Microsoft.EntityFrameworkCore;
-using OpusLink.Entity.Automapper;
-
 internal class Program
 {
     private static void Main(string[] args)
@@ -9,26 +5,14 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        builder.Services.AddControllersWithViews();
+
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        //builder.Services.AddDbContext<PE_PRN_Fall2023B1Context>(option =>
-        //{
-        //    option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings"));
-        //});
-
-        var mapperConfig = new MapperConfiguration(mc =>
-        {
-            mc.AddProfile(new MapperConfig());
-        });
-        IMapper mapper = mapperConfig.CreateMapper();
-        builder.Services.AddSingleton(mapper);
-
         var app = builder.Build();
-        //builder.Services.AddAutoMapper(typeof(ApplicationMapper));
+
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
