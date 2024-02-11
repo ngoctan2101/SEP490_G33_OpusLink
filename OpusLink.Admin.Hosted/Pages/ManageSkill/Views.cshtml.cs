7d0b7ac77cc98350a34101d6605b0a75e2af624e
ngoctan2101 <109189357+ngoctan2101@users.noreply.github.com>
2024-02-11 21:13:45 +0700

    View Page ManageSkill, ManageUser

---
+++ OpusLink.Admin.Hosted/Pages/ManageSkill/Views.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpusLink.Admin.Hosted.Pages.ManageSkill
+{
+    public class ViewsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ OpusLink.Admin.Hosted/Pages/ManageUser/Views.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpusLink.Admin.Hosted.Pages.ManageUser
+{
+    public class ViewsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
