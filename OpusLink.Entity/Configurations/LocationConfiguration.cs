54889622a7d5b4ab4f4c858a42a337f53d09b21a
tsn0710 <tongnhat07102001@gmail.com>
2024-02-16 19:11:16 +0700

    Seed data 2, Database 3

--- OpusLink.Entity/Configurations/LocationConfiguration.cs
+++ OpusLink.Entity/Configurations/LocationConfiguration.cs
@@ -1,11 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using OpusLink.Entity.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpusLink.Entity.Configurations
 {
@@ -18,8 +13,28 @@ namespace OpusLink.Entity.Configurations
             builder.Property(x => x.LocationID).ValueGeneratedOnAdd();
             builder.Property(x => x.LocationName).IsRequired().HasMaxLength(256);
             builder.HasData(
-                new Location { LocationID = 1, LocationName = "HaNoi" },
-                new Location { LocationID = 2, LocationName = "DaNang" }
+                new Location { LocationID = 1, LocationName = "Hà Giang" },
+new Location { LocationID = 2, LocationName = "Cao Bằng" },
+new Location { LocationID = 3, LocationName = "Lào Cai" },
+new Location { LocationID = 4, LocationName = "Sơn La" },
+new Location { LocationID = 5, LocationName = "Lai Châu" },
+new Location { LocationID = 6, LocationName = "Bắc Kạn" },
+new Location { LocationID = 7, LocationName = "Lạng Sơn" },
+new Location { LocationID = 8, LocationName = "Tuyên Quang" },
+new Location { LocationID = 9, LocationName = "Yên Bái" },
+new Location { LocationID = 10, LocationName = "Thái Nguyên" },
+new Location { LocationID = 11, LocationName = "Điện Biên" },
+new Location { LocationID = 12, LocationName = "Phú Thọ" },
+new Location { LocationID = 13, LocationName = "Vĩnh Phúc" },
+new Location { LocationID = 14, LocationName = "Bắc Giang" },
+new Location { LocationID = 15, LocationName = "Bắc Ninh" },
+new Location { LocationID = 16, LocationName = "Hà Nội" },
+new Location { LocationID = 17, LocationName = "Quảng Ninh" },
+new Location { LocationID = 18, LocationName = "Hải Dương" },
+new Location { LocationID = 19, LocationName = "Hải Phòng" },
+new Location { LocationID = 20, LocationName = "Hòa Bình" },
+new Location { LocationID = 21, LocationName = "Hưng Yên" }
+
                 );
         }
     }
--- OpusLink.Entity/Migrations/20240216120635_InitialDB.Designer.cs
+++ OpusLink.Entity/Migrations/20240216120635_InitialDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OpusLink.Entity;
 
@@ -11,9 +12,10 @@ using OpusLink.Entity;
 namespace OpusLink.Entity.Migrations
 {
     [DbContext(typeof(OpusLinkDBContext))]
-    partial class OpusLinkDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240216120635_InitialDB")]
+    partial class InitialDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
