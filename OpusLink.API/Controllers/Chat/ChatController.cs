afce1a91aeeb2343198b1330467007021dd43b08
thanhnthe1 <91208887+thanhnthe1@users.noreply.github.com>
2024-02-19 23:16:29 +0700

    update chat api

---
+++ OpusLink.API/Controllers/Chat/ChatController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using OpusLink.Entity.DTO;
+using OpusLink.Entity.Models;
+using OpusLink.Service.Admin;
+using OpusLink.Service.Chat;
+
+namespace OpusLink.API.Controllers.Chat
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ChatController : ControllerBase
+    {
+        readonly IChatService _chatService;
+        private IMapper _mapper;
+        public ChatController(IChatService chatService, IMapper mapper)
+        {
+            _chatService = chatService;
+            _mapper = mapper;
+        }
+        [HttpGet("GetAllChat")]
+        public IActionResult GetAllChat()
+        {
+            var chat = _chatService.getAllChatBox();
+            if (chat != null && chat.Count == 0)
+            {
+                return Ok("Don't have chat");
+            }
+            return Ok(_mapper.Map<ChatBox>(chat));
+        }
+    }
+}
---
+++ OpusLink.Service/Chat/ChatService.cs
@@ -0,0 +1,52 @@
+﻿using OpusLink.Entity;
+using OpusLink.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpusLink.Service.Chat
+{
+    public interface IChatService
+    {
+        List<ChatBox> getAllChatBox();
+        ChatBox getChatBox(int id);
+
+        public class ChatService : IChatService
+        {
+            private readonly OpusLinkDBContext _context;
+            public ChatService (OpusLinkDBContext context)
+            {
+                context = _context;
+            }
+            public List<ChatBox> getAllChatBox()
+            {
+                try
+                {
+                    var chatBox = _context.ChatBoxs.ToList();
+                    return chatBox;
+
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+
+            public ChatBox getChatBox(int id)
+            {
+                try
+                {
+                    var chatBox = _context.ChatBoxs.FirstOrDefault(x => x.ChatBoxID == id);
+                    return chatBox;
+
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+    }
+}
