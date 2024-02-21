90ddae97749e49147f184753e36927ff33f2ee13
thanhnthe1 <91208887+thanhnthe1@users.noreply.github.com>
2024-02-21 23:16:32 +0700

    what?

--- OpusLink.User.Hosted/Pages/Chat/ChatList.cshtml.cs
+++ OpusLink.User.Hosted/Pages/Chat/ChatList.cshtml.cs
@@ -1,15 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OpusLink.Entity.DTO;
 using OpusLink.Entity.Models;
 using OpusLink.Entity.Models.JOB;
 using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
 
 namespace OpusLink.User.Hosted.Pages.Chat
 {
     public class ChatListModel : PageModel
     {
         private readonly HttpClient client = null;
-        public IList<ChatBox> ChatBoxes { get; set; } = default;
+        public IList<ChatDTO> ChatDTOs { get; set; } = default;
         public ChatListModel()
         {
             client = new HttpClient();
@@ -18,27 +21,19 @@ namespace OpusLink.User.Hosted.Pages.Chat
         }
         public async Task OnGetAsync()
         {
-            HttpResponseMessage response = await client.GetAsync("https://localhost:7265/api/Job3API/GetAllJob");
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7265/api/ChatController/GetAllChat");
 
-            response = await client.GetAsync("https://localhost:7265/api/Job3API/GetAllChildCategory?parentId=" + 0);
+            //response = await client.GetAsync("https://localhost:7265/api/ChatController/GetAllChildCategory?parentId=" + 0);
             if (response.IsSuccessStatusCode)
             {
                 string strData = await response.Content.ReadAsStringAsync();
-                ChatBoxes = JsonConvert.DeserializeObject<List<GetJobResponse>>(strData);
+                ChatDTOs = JsonConvert.DeserializeObject<List<ChatDTO>>(strData);
             }
             else
             {
 
             }
-            if (response.IsSuccessStatusCode)
-            {
-                string strData = await response.Content.ReadAsStringAsync();
-                ChatBoxes = JsonConvert.DeserializeObject<List<ChatBox>>(strData);
-            }
-            else
-            {
-
-            }
+            
         }
     }
 }
