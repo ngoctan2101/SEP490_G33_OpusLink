fc6e151eeffb1434e4990792e010e2dd04fbfa84
tsn0710 <tongnhat07102001@gmail.com>
2024-03-01 02:05:13 +0700

    done Admin edit user profile.

---
+++ OpusLink.Entity/AutoMapper/JOB/UserProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using OpusLink.Entity.DTO.JobDTO;
+using OpusLink.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpusLink.Entity.AutoMapper.JOB
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<PutUserRequest, User>()
+                .ForMember(dest => dest.Id, src => src.MapFrom(x => x.Id))
+                .ForMember(dest => dest.Email, src => src.MapFrom(x => x.Email))
+                .ForMember(dest => dest.PhoneNumber, src => src.MapFrom(x => x.PhoneNumber))
+                .ForMember(dest => dest.Address, src => src.MapFrom(x => x.Address))
+                .ForMember(dest => dest.FullNameOnIDCard, src => src.MapFrom(x => x.FullNameOnIDCard))
+                .ForMember(dest => dest.IDNumber, src => src.MapFrom(x => x.IDNumber))
+                .ForMember(dest => dest.Dob, src => src.MapFrom(x => x.Dob))
+                .ForMember(dest => dest.Introduction, src => src.MapFrom(x => x.Introduction))
+                .ForMember(dest => dest.BankName, src => src.MapFrom(x => x.BankName))
+                .ForMember(dest => dest.BankAccountInfor, src => src.MapFrom(x => x.BankAccountInfor))
+                .ForMember(dest => dest.Status, src => src.MapFrom(x => x.Status));
+        }
+    }
+}
---
+++ OpusLink.Entity/DTO/JobDTO/PutUserRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpusLink.Entity.DTO.JobDTO
+{
+    public  class PutUserRequest
+    {
+        public int Id { get; set; }
+        //public string UserName { get; set; }
+        public string? Email { get; set; }
+        //public string? ProfilePicture { get; set; }
+        public byte[]? UserImageBytes { get; set; } = null;
+        public string? imageExtension { get; set; }
+        public byte[]? UserCVBytes { get; set; } = null;
+        public string? cvExtension { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? FullNameOnIDCard { get; set; }
+        public string? IDNumber { get; set; }
+        public DateTime? Dob { get; set; }
+        //public decimal? StarMedium { get; set; }
+        public string? Introduction { get; set; }
+        //public string? CVFilePath { get; set; }
+        public string? BankName { get; set; }
+        public string? BankAccountInfor { get; set; }
+        //public bool IsVeryfiedIdentity { get; set; }
+        //public decimal? AmountMoney { get; set; }
+        public int Status { get; set; }
+
+        public List<Int32> SkillIDs { get; set; } = new List<Int32>();
+    }
+}
