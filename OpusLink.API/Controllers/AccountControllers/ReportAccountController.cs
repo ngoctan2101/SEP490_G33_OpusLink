44022c59873c3372e6b0f6c001cbe208975f1d63
haind <duchainguyen1004@gmail.com>
2024-03-27 16:20:53 +0700

    Update AddReport
    Update AddReport

---
+++ OpusLink.API/Controllers/AccountControllers/ReportAccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpusLink.Entity;
+using OpusLink.Entity.DTO.AccountDTO;
+using OpusLink.Entity.DTO.ReportUserDTO;
+using OpusLink.Entity.Models;
+
+namespace OpusLink.API.Controllers.AccountControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportAccountController : ControllerBase
+    {
+        private readonly OpusLinkDBContext _context;
+
+        public ReportAccountController(OpusLinkDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AddNewReport(ReportAccountDTO reportUser)
+        {
+            ReportUser reportAccount = new ReportUser()
+            {
+                CreateByUserID = reportUser.CreateByUserID,
+                TargetToUserID = reportUser.TargetToUserID,
+                ReportUserContent = reportUser.ReportUserContent,
+                DateCreated = DateTime.Now
+            };
+            _context.ReportUsers.Add(reportAccount);
+            _context.SaveChanges();
+            return Ok("Add report successfull");
+        }
+    }
+}
---
+++ OpusLink.Entity/DTO/AccountDTO/ReportAccountDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpusLink.Entity.DTO.AccountDTO
+{
+    public class ReportAccountDTO
+    {
+        public int CreateByUserID { get; set; }
+        public int TargetToUserID { get; set; }
+        public string ReportUserContent { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
