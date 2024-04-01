ed408e820e6c0ea6c02517c6c7ebf4f3eb718c79
haind <duchainguyen1004@gmail.com>
2024-04-02 02:43:35 +0700

    Update Report
    Update Report

--- OpusLink.API/Controllers/AccountControllers/ReportAccountController.cs
+++ OpusLink.API/Controllers/AccountControllers/ReportAccountController.cs
@@ -20,21 +20,36 @@ namespace OpusLink.API.Controllers.AccountControllers
         [HttpPost]
         public async Task<ApiResponseModel> AddNewReport(ReportAccountDTO reportAccountDTO)
         {
-            ReportUser reportAccount = new ReportUser()
+            var job = _context.Jobs.FirstOrDefault(j => j.EmployerID == reportAccountDTO.CreateByUserID && j.FreelancerID == reportAccountDTO.TargetToUserID ||
+            j.EmployerID == reportAccountDTO.TargetToUserID && j.FreelancerID == reportAccountDTO.CreateByUserID);
+
+            if (job == null)
             {
-                CreateByUserID = reportAccountDTO.CreateByUserID,
-                TargetToUserID = reportAccountDTO.TargetToUserID,
-                ReportUserContent = reportAccountDTO.ReportUserContent,
-                DateCreated = DateTime.Now
-            };
-            _context.ReportUsers.Add(reportAccount);
-            int number = _context.SaveChanges();
-            return new ApiResponseModel
+                return new ApiResponseModel
+                {
+                    Code = 400,
+                    Message = "Bạn không được quyền Report tài khoản này",
+                    IsSuccess = false
+                };
+            }
+            else
             {
-                Code = 200,
-                Message = "Success",
-                IsSuccess = true
-            };
+                ReportUser reportAccount = new ReportUser()
+                {
+                    CreateByUserID = reportAccountDTO.CreateByUserID,
+                    TargetToUserID = reportAccountDTO.TargetToUserID,
+                    ReportUserContent = reportAccountDTO.ReportUserContent,
+                    DateCreated = DateTime.Now
+                };
+                _context.ReportUsers.Add(reportAccount);
+                int number = _context.SaveChanges();
+                return new ApiResponseModel
+                {
+                    Code = 200,
+                    Message = "Success",
+                    IsSuccess = true
+                };
+            }
         }
     }
 }
