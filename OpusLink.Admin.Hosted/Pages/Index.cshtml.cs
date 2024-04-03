9afaef17be89e9b7caef35306fdec8b3267d72f9
haind <duchainguyen1004@gmail.com>
2024-04-03 19:15:48 +0700

    Update Notification
    Update Notification

--- OpusLink.Admin.Hosted/Pages/Index.cshtml.cs
+++ OpusLink.Admin.Hosted/Pages/Index.cshtml.cs
@@ -12,9 +12,22 @@ namespace OpusLink.Admin.Hosted.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+/*        public void OnGet()
         {
             HttpContext.Session.SetString("PageNow", "Index");
+        }*/
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            string token = HttpContext.Session.GetString("token");
+            if (token == null)
+            {
+                return RedirectToPage("/Account/Login");
+            }
+            else
+            {
+                return RedirectToPage("/Dashboard/DashboardAdmin");
+            }
         }
     }
 }
