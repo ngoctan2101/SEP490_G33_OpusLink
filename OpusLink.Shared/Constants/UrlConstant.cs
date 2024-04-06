ba1b8a57c771adb08a620e1f9e3a0b07d29e9e0d
tsn0710 <tongnhat07102001@gmail.com>
2024-04-06 20:02:36 +0700

    change url2

--- OpusLink.Shared/Constants/UrlConstant.cs
+++ OpusLink.Shared/Constants/UrlConstant.cs
@@ -9,7 +9,7 @@ namespace OpusLink.Shared.Constants
     public class UrlConstant
     {
         //public const string ApiBaseUrl = "https://localhost:7277";
-        public const string ApiBaseUrl = "https://localhost:7265/api";
-        public const string UserClientBaseUrl = "https://localhost:7131";
+        public const string ApiBaseUrl = "https://refined-mutually-frog.ngrok-free.app/api";//https://localhost:7265/api
+        public const string UserClientBaseUrl = "https://localhost:7131";//https://localhost:7131
     }
 }
