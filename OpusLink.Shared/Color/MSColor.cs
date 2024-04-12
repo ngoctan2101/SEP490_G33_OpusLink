eef92064f20a56e4ee9a429bc8841e829806e4f0
tsn0710 <tongnhat07102001@gmail.com>
2024-04-13 06:37:28 +0700

    Màu cho MS status, Icon cho button, format cho input Date

---
+++ OpusLink.Shared/Color/MSColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpusLink.Shared.Color
+{
+    public class MSColor
+    {
+        public const string New = "#32CD32";
+        public const string WaitingForReview = "#FF8500";
+        public const string WaitingForReviewAgain = "#FF8500";
+        public const string Late = "#8B0000";
+        public const string EmployerLate = "#8B0000";
+        public const string FreelancerLate = "#8B0000";
+        public const string MoneyPutted = "#1E90FF";
+        public const string FreelancerReDo = "#1E90FF";
+        public const string Completed = "#008000";
+        public const string Failed = "#FF0000";
+
+    }
+}
